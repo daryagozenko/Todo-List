dfc59aa4bcec1f1aea30c1e53220347db8074ba7
Дарья <gozenko.dasha@bk.ru>
2026-03-23 00:02:32 +0400

    fix docker-compose

--- TodoList/src/main/java/com/gozenko/TodoList/TodoListApplication.java
+++ TodoList/src/main/java/com/gozenko/TodoList/TodoListApplication.java
@@ -3,11 +3,11 @@ package com.gozenko.TodoList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class TodoListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoListApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(TodoListApplication.class, args);}
 
 }
